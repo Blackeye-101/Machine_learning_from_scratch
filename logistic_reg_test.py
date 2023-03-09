9aa293f64eee63ee8b1c4cae93c7d9c3df217677
unknown <rishabh.th25@gmail.com>
2023-03-09 20:19:18 +0530

    adding logistic regression

---
+++ logistic_reg_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+from logistic_regression import LogisticRegression
+
+bc=datasets.load_breast_cancer()
+X,y=bc.data, bc.target
+
+X_train, X_test,y_train, y_test=train_test_split(X,y, test_size=0.2, random_state=1234)
+
+
+regressor=LogisticRegression(lr=0.001, n_iterations=1000)
+regressor.fit(X_train, y_train)
+predictions=regressor.predict(X_test)
+
+accuracy=np.sum(predictions==y_test)/len(y_test)
+print(accuracy)
